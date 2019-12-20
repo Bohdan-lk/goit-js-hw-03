2ffd89ac73a33f042a31cfbbb87b3f86fce46e5d
bogdan <bogdan.lysenko.k@gmail.com>
2019-12-20 14:58:37 +0200

    v8

---
+++ js/task-1.js
@@ -0,0 +1,19 @@
+"use strict";
+
+const user = {
+    name: 'Mango',
+    age: 20,
+    hobby: 'html',
+    premium: true,
+};
+
+user.mood = 'happy';
+user.hobby = 'skydiving';
+user.premium = false;
+
+const users = Object.keys(user);
+console.table(users);
+for (const el of users) {
+    console.log(`${el} -`,user[el])
+}
+
